e8a776cd7d9ce0538f3722ce6cbae6f4cef8a205
patrykbrodziak <patryk.brodziak1@gmail.com>
2020-07-20 21:30:05 +0200

    final course

--- mysql_connect.py
+++ mysql_connect.py
@@ -21,9 +21,37 @@ mycursor = mydb.cursor()
 #             ("Michelle", 17)]
 # mycursor.executemany(sql_formula, students)
 # mydb.commit()
+
+
+
 # mycursor.execute("SELECT age FROM students")
 # my_result = mycursor.fetchall()
 # my_result = mycursor.fetchone()
 # for row in my_result:
 #     print(row)
 
+# sql = "SELECT * FROM students WHERE name LIKE '%ac%'"
+# sql = "SELECT * FROM students WHERE name =%s"
+# mycursor.execute(sql, ("Michelle", ))
+# myresult = mycursor.fetchall()
+# for result in myresult:
+#     print(result)
+
+# sql = "SELECT * FROM students ORDER BY name DESC"
+# sql = "UPDATE students SET age =13 WHERE name ='Bob'"
+
+# mycursor.execute("SELECT * FROM students LIMIT 5 OFFSET 2") #zacznij od 2
+# myresult = mycursor.fetchall()
+# for result in myresult:
+#     print(result)
+
+# sql = "SELECT * FROM students ORDER BY age"
+# mycursor.execute(sql)
+# myresult = mycursor.fetchall()
+# for result in myresult:
+#     print(result)
+
+# sql = "DELETE FROM students WHERE age = 13"
+sql = "DROP TABLE IF EXISTS students"
+mycursor.execute(sql)
+mydb.commit()
